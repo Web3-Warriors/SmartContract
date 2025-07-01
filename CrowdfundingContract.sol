9f102c24d4f5642ebcd9db333850966290c74b8a
Hannnzchool <laxus748799@gmail.com>
2025-07-01 17:26:03 +0700

    Feat: Add view function for programs

--- CrowdfundingContract.sol
+++ CrowdfundingContract.sol
@@ -1,203 +1,98 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessContr/ol.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
-import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
+contract crowdFunding is AccessControl{
 
-contract CrowdFundingContract is Ownable{
+    bytes32 public constant PIC_ROLE = keccak256("PIC_ROLE");
 
+    constructor(){
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
 
-    constructor() Ownable(msg.sender) {}
-
-    uint256 private _nextProgramId;
+    uint256 private _nextId = 1;
 
     enum ProgramStatus {Active, Completed, Canceled}
 
     struct Program {
         uint256 id;
-        string  title;
-        string  image;
-        string  desc;
+        string title;
+        string desc;
+        string image;
         address pic;
         uint256 targetFund;
         uint256 startDate;
         uint256 endDate;
-        uint256 totalAmount;
-        uint256 withdrawAmount;
+        uint256 amountNow;
         ProgramStatus status;
-        // Contribute[] contributions;
-        // address[] contributors;
-        // uint[] contributionAmounts;
-    }
-
-    struct ContributeHistory {
-        address contribute;
-        uint256 amount;
-    }
-    
-
-    struct Withdrawal {
-        uint256 programId;
-        uint256 amount;
-        string desc;
-        uint256 time;
+        address[] contributors;
+        uint[] contributionAmounts;
     }
 
     mapping(uint256 => Program) public programs;
-    // contribute history by Program
-    mapping(uint256 => ContributeHistory[]) public contributeHistory;
-    // history withdraw by Program
-    mapping(uint256 => Withdrawal[]) public withdrawalByProgram;
 
-    Program[] public listProgram;
-
-
-    modifier onlyPIC(uint256 _programId ,address _PIC){
-        if(programs[_programId].pic != _PIC ){
-            revert CallerNotPIC();
-        }
+    modifier onlyAdmin(){
+        require(hasRole(DEFAULT_ADMIN_ROLE, msg.sender), "Not admin");
         _;
     }
 
-    error ProgramEnd();
-    error CallerNotPIC();
-    error FundraiseIsNotClosed();
-    error WithdrawAmountError();
-    error WithdrawFailed();
-    error CancelAndRefundFailed();
+    Program[] public listProgram;
+    uint256[] public listTokenId;
 
-    function createProgram
-        (
-        string memory _title,
-        string memory _desc,
-        string memory _image,
-        address _pic,
-        uint256 _targetFund,
-        uint256 _startDate,
-        uint256 _endDate        
-        )
-        public 
-        onlyOwner 
-        {
-            uint256 programId = ++_nextProgramId;
-            programs[programId] = Program({
-                id                  : programId,
-                title               :_title,
-                desc                :_desc,
-                image               :_image, 
-                pic                 : _pic, 
-                targetFund          : _targetFund, 
-                startDate           : _startDate, 
-                endDate             : _endDate, 
-                totalAmount         : 0,
-                withdrawAmount      : 0,
-                status              : ProgramStatus.Active
+    function createProgram(
+            string memory _title,
+            string memory _desc,
+            string memory _image,
+            address _pic,
+            uint256 _targetFund,
+            uint256 _startDate,
+            uint256 _endDate
+            )
+        public onlyAdmin {
+            programs[_nextId] = Program({
+                id: _nextId,
+                title:_title,
+                desc :_desc,
+                image :_image, 
+                pic   : _pic, 
+                targetFund     : _targetFund, 
+                startDate      : _startDate, 
+                endDate        : _endDate, 
+                amountNow          : 0,
+                status         : ProgramStatus.Active,
+                contributors: new address[] (0),
+                contributionAmounts: new uint[] (0)
             });
 
-            listProgram.push(programs[programId]);
-
-    }
-
-    function contribute
-        (
-            uint _programId
-        )
-        public
-        payable
-        {
-            Program storage _program = programs[_programId];
-
-            require(_program.status == ProgramStatus.Active, "Not Active");
-            if(block.timestamp > _program.endDate ) {
-                revert ProgramEnd();
-            }
-            // validasi untuk dana program sudah tercapai
-            require(_program.totalAmount != _program.targetFund, "Fund has reached");
-            
-            // mengubah status menjadi completed jika fund terakhir sudah mencukupi target fund
-            if((msg.value + _program.totalAmount) == _program.targetFund){
-                _program.status = ProgramStatus.Completed;
-            }
-           
-            _program.totalAmount += msg.value;
-
-            ContributeHistory memory _contribute = ContributeHistory(
-                msg.sender,
-                msg.value
-            );
-            contributeHistory[_programId].push(_contribute);
-    }
-
-    function withdraw
-        (
-            uint256 _programId, 
-            uint256 _amount,
-            string memory _desc
-        )
-        public
-        onlyPIC(_programId, msg.sender)
-        {
-            Program storage _program = programs[_programId];
-            if (_program.targetFund > block.timestamp) {
-                revert FundraiseIsNotClosed();
-            
-            }
-
-            uint256 totaAmountFund = _program.totalAmount;
-            uint256 totalWithdraw = _program.withdrawAmount;
-
-            // withdraw tidak boleh lebih dari data total dana yang sudah terkumpul
-            if (_amount + totalWithdraw >= totaAmountFund) {
-                revert WithdrawAmountError();
-            }
-
-            _program.withdrawAmount += _amount;
-            (bool success, ) = payable(_program.pic).call{value: _amount}("");
-            if(!success) revert WithdrawFailed();
-
-            Withdrawal memory withdrawal = Withdrawal(
-                _programId,
-                _amount,
-                _desc,
-                block.timestamp
-            );
-
-            withdrawalByProgram[_programId].push(withdrawal);
-
-    }
-
-    function cancelAndRefund
-        (
-            uint256 _programId
-        ) 
-        public 
-        onlyOwner 
-        {
-            Program storage _program = programs[_programId];
-            require(
-                _program.status == ProgramStatus.Active || _program.status == ProgramStatus.Completed,
-                "Not Active or Completed"
-            );
-            for (uint i = 0; i < contributeHistory[_programId].length; i++) {
-                address _addr = contributeHistory[_programId][i].contribute;
-                uint256 _amount = contributeHistory[_programId][i].amount;
-                (bool success, ) = payable(_addr).call{value: _amount}("");
-                if(!success) revert CancelAndRefundFailed();
-            }
-            _program.status = ProgramStatus.Canceled;
-    }
-
-
-    function getHistoryWithdrawByProgram
-        (
-            uint256 _programId
-        )
-        public 
-        view
-        returns(Withdrawal[] memory)
-        {
-            return withdrawalByProgram[_programId];
+            listProgram.push(programs[_nextId]);
+            listTokenId.push(_nextId);
+            _nextId++;
+            _grantRole(PIC_ROLE, _pic);
         }
+
+        function contribute(uint _id, uint256 amount) public payable {
+        Program storage programStorage = programs[_id];
+
+        uint256 remainingFundsNeeded = programStorage.targetFund - programStorage.amountNow;
+
+        if(amount < remainingFundsNeeded){
+            programStorage.amountNow += amount;
+        }
+        else if(amount == remainingFundsNeeded){
+            programStorage.amountNow += amount;
+            programStorage.status = ProgramStatus.Completed;
+        }
+        else{
+            uint excessAmount = amount - remainingFundsNeeded;
+            uint refundedAmount = amount - excessAmount;
+            payable(msg.sender).transfer(excessAmount);
+            programStorage.amountNow += refundedAmount;
+            programStorage.status = ProgramStatus.Completed;
+        }
+        programStorage.contributors.push(msg.sender);
+        programStorage.contributionAmounts.push(amount);
+    } 
     
 }
